d254c427665a67e2110825b03b1f27e34f8eb6b8
harry <harry0developer@gmail.com>
2016-09-07 08:03:35 +0200

    Buxs fixed

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -109,5 +109,29 @@ angular.module('starter.payPalService', [])
 			return defer.promise;
 		}
 		return service;
+}])
+
+app.factory('userService', ['$rootScope', function ($rootScope) {
+
+    var service = {
+
+        model: {
+            name: '',
+            email: ''
+        },
+
+        SaveState: function () {
+            sessionStorage.userService = angular.toJson(service.model);
+        },
+
+        RestoreState: function () {
+            service.model = angular.fromJson(sessionStorage.userService);
+        }
+    }
+
+    $rootScope.$on("savestate", service.SaveState);
+    $rootScope.$on("restorestate", service.RestoreState);
+
+    return service;
 }]);
 	
